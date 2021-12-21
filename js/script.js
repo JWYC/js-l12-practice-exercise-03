25ce8d2c238d4475a33d2b03f6ea9a1adee809e5
jwyc <janis.wy.chan@gmail.com>
2021-12-21 09:11:31 -0800

    created list elements with name and append to not- registered element in html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const nonRegistered = document.querySelector(".non-registered");
+const nonRegistered = document.querySelector(".not-registered");
 
 const getRegistrationData = async function(){
    const request = await fetch ("https://gist.githubusercontent.com/skillcrush-curriculum/4bfa96d3762f86d2327d389063b3ea8d/raw/86e50e48e98c662ead6d9bd191ef7db381e9f4b4/fieldtrip.json"
@@ -8,14 +8,21 @@ const getRegistrationData = async function(){
    displayContactList(data);
 }
 
-getRegistrationData();
 
 const displayContactList = function(data){
    for (let student of data){
       if (student.registered == "no"){
-         console.log(student.name);
+         //console.log(student.name);
+         
+         let name = student.name
+         
+         let list = document.createElement("li");
+         list.innerText = name;
+         //console.log(list);
+         nonRegistered.append(list);
       }
-      
    }
 }
 
+
+getRegistrationData();
